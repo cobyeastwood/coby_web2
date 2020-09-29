dec963b753a6a4bde6de5995e6eded6573f741ee
cobyeast <cobyeastwood@gmail.com>
2020-09-28 22:59:40 -0700

    adding controllers package

--- main.go
+++ main.go
@@ -1,30 +1,27 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/cobyeastwood/coby_web/controllers"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	router := gin.Default()
+func server(router *gin.Engine, port string) {
 
 	router.Use(static.Serve("/", static.LocalFile("./client/build", true)))
 
 	api := router.Group("/api")
-	{
-		api.GET("/pong", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "pong",
-			})
-		})
-		api.GET("/ping", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "ping",
-			})
-		})
-	}
 
-	router.Run(":8080")
+	controllers.GetRoutes(api)
+	controllers.PostRoutes(api)
+
+	router.Run(":" + port)
+
+}
+
+func main() {
+
+	router := gin.Default()
+
+	server(router, "8080")
 }
