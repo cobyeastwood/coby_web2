b423f159ef2f63e981c97a7bc1f57bbfda838a7b
cobyeast <cobyeastwood@gmail.com>
2020-09-30 19:27:10 -0700

    static files served on all routes

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ WORKDIR /coby_web
 
 COPY . .
 
-RUN go get -u github.com/gin-gonic/gin github.com/gin-gonic/contrib/static github.com/gin-gonic/contrib/cors
+RUN go get -u github.com/go-chi/chi github.com/go-chi/chi/middleware
 RUN go build -o main .
 
 EXPOSE 8080
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -16,14 +18,16 @@ type Data struct {
 	Payload string `json:"payload"`
 }
 
+func runn() {
+	fmt.Println("hello")
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	msg1 := Data{"ping", "test"}
 	msg2 := Data{"pong", "test"}
-
-	workDir, _ := os.Getwd()
 
 	r.Get("/api/pong", func(w http.ResponseWriter, r *http.Request) {
 		// w.Header().Set("Content-Type", "application/json")
@@ -35,28 +39,39 @@ func main() {
 		json.NewEncoder(w).Encode(msg2)
 	})
 
-	filesDir := http.Dir(filepath.Join(workDir, "./client/build"))
+	fileServer(r, "./client/build")
 
-	fileServer(r, "/", filesDir)
+	runn()
 
 	http.ListenAndServe(":8080", r)
 }
 
-func fileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("fileServer does not permit any URL parameters.")
+func fileServer(r chi.Router, static string) {
+
+	public := "/"
+
+	if strings.ContainsAny(public, "{}*") {
+		panic("FileServer does not permit URL parameters.")
 	}
 
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
+	root, _ := filepath.Abs(static)
 
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+	if _, err := os.Stat(root); os.IsNotExist(err) {
+		panic("Static Documents Directory Not Found")
+	}
+
+	fs := http.StripPrefix(public, http.FileServer(http.Dir(root)))
+
+	if public != "/" && public[len(public)-1] != '/' {
+		r.Get(public, http.RedirectHandler(public+"/", 301).ServeHTTP)
+		public += "/"
+	}
+
+	r.Get(public+"*", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		file := strings.Replace(r.RequestURI, public, "/", 1)
+		if _, err := os.Stat(root + file); os.IsNotExist(err) {
+			http.ServeFile(w, r, path.Join(root, "index.html"))
+		}
 		fs.ServeHTTP(w, r)
-	})
+	}))
 }
